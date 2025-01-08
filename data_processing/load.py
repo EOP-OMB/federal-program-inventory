f73b50cef346a889e6f7c83656adf8a8919db13e
William-Yu1 <88672167+William-Yu1@users.noreply.github.com>
2025-01-08 09:31:41 -0500

    update load to load all data

--- data_processing/load.py
+++ data_processing/load.py
@@ -1361,25 +1361,25 @@ try:
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
-    # programs_data = generate_program_data(cursor, FISCAL_YEARS)
+    programs_data = generate_program_data(cursor, FISCAL_YEARS)
 
     shared_data = generate_shared_data(cursor)
 
-    # generate_program_markdown_files(MARKDOWN_DIR, programs_data, FISCAL_YEARS)
+    generate_program_markdown_files(MARKDOWN_DIR, programs_data, FISCAL_YEARS)
 
-    # generate_program_csv('../website/assets/files/all-program-data.csv', programs_data, FISCAL_YEARS)
+    generate_program_csv('../website/assets/files/all-program-data.csv', programs_data, FISCAL_YEARS)
     
     search_path = os.path.join('../website', 'pages', 'search.md')
     generate_search_page(search_path, shared_data, FISCAL_YEARS[0])
     
-    # category_path = os.path.join('../website', 'pages', 'category.md')
-    # generate_category_page(cursor, programs_data, category_path, FISCAL_YEARS[0])
+    category_path = os.path.join('../website', 'pages', 'category.md')
+    generate_category_page(cursor, programs_data, category_path, FISCAL_YEARS[0])
     
     home_path = os.path.join('../website', 'pages', 'home.md')
     generate_home_page(home_path, shared_data, FISCAL_YEARS[0])
     
-    # programs_json_path = os.path.join('../elasticsearch-custom', 'data', 'programs-table.json')
-    # generate_programs_table_json(programs_json_path, programs_data, FISCAL_YEARS[0])
+    programs_json_path = os.path.join('../elasticsearch-custom', 'data', 'programs-table.json')
+    generate_programs_table_json(programs_json_path, programs_data, FISCAL_YEARS[0])
     
     category_dir = os.path.join('../website', '_category')
     generate_category_markdown_files(cursor, category_dir, FISCAL_YEARS[0])
